f3d48e1654fa4fd3a432ca79d7460c4b1a5d01c1
cirosalzmann <sciro@student.ethz.ch>
2016-11-20 20:01:04 +0100

    Update Initialise.m

--- code/Initialise.m
+++ code/Initialise.m
@@ -4,7 +4,7 @@ function [Opinion, Strength, Map] = Initialise(SizeX, SizeY, maxRange)
 %     Opinion = zeros(SizeX, SizeY);
 
     Map = round(mat2gray(rgb2gray(imresize(imread('Map.jpg'),[SizeX SizeY]))));
-    Opinion = mat2gray(rgb2gray(imresize(imread('Test.jpg'),[SizeX SizeY])))*2-1;
+    Opinion = mat2gray(rgb2gray(imresize(imread('OpMap.jpg'),[SizeX SizeY])))*2-1;
 
     
     for i = 1:SizeX
