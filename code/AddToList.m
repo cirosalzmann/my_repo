dd8c2463cf1572506010073e1a9b61c31ae2f8b4
cirosalzmann <sciro@student.ethz.ch>
2016-11-20 19:59:20 +0100

    Delete AddToList.m

--- code/AddToList.m
+++
@@ -1,9 +0,0 @@
-function List = AddToList(currList, Value)
-    newList = zeros(1, length(currList));
-    
-    for i = 1:length(currList)
-       newList(i) = currList(i); 
-    end
-    newList(length(currList) + 1) = Value;
-    List = newList;
-end
